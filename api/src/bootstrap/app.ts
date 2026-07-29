408a5b6dd7512f3485024a90273bf6d7982bd20e
Nithin <nithinvadakkethil18@gmail.com>
2026-07-29 07:43:21 -0700

    feat: set server request body limit to 10mb

--- api/src/bootstrap/app.ts
+++ api/src/bootstrap/app.ts
@@ -22,8 +22,8 @@ export function createApp(): express.Express {
   );
   app.use(cors());
   app.use(compression());
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.use(express.json({ limit: '10mb' }));
+  app.use(express.urlencoded({ limit: '10mb', extended: true }));
   app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
   app.use(httpLogger);
 
